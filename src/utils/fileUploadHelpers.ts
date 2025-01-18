570602db57f3ad493711cb74317b25844f589edc
alamshuvo <iftakharalamshuvo11@gmail.com>
2025-01-18 18:51:47 +0600

    cloudinary image path name added

--- src/utils/fileUploadHelpers.ts
+++ src/utils/fileUploadHelpers.ts
@@ -1,5 +1,6 @@
 import multer from 'multer'
-import path from 'path'
+import path, { resolve } from 'path'
+import { v2 as cloudinary } from 'cloudinary'
 const storage = multer.diskStorage({
     destination:(req,file,cb)=>{
       cb(null,"./uploads")
@@ -14,3 +15,16 @@ const storage = multer.diskStorage({
 })
 
 export const  upload =multer({storage:storage})
+cloudinary.config({cloud_name:"dqugrb0la",api_key:"873695859846382",api_secret:"cVbocNrFEicAWKXP5PIjiPCVtb8"})
+
+
+export const sendImageCloudinary = (imageName:string,path:string)=>{
+ return new Promise((resolve,rejects)=>{
+  cloudinary.uploader.upload(path,{public_id:imageName.trim()},(error,result)=>{
+    if (error) {
+      rejects(error)
+    }
+    resolve(result)
+  })
+ })
+}
